e4f2a4d302129d0333158186cf335cd066ca27b5
ramimhossain12 <ramimhossain6242@gmail.com>
2020-09-05 20:27:56 +0600

    Removeing view by id

--- app/src/main/java/com/example/databiinding/MainActivity.java
+++ app/src/main/java/com/example/databiinding/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.databiinding;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.databiinding.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding =
+                DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+        
+
+        binding.tvOne.setText("Ramim");
+        binding.tvTwo.setText("Hossain");
+        binding.tvThree.setText("Hridoy");
+        binding.btnID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
     }
 }
